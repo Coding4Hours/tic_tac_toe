59797d6a04e7d967406bd83d60bea27d38f748b0
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-07-09 10:09:16 -0700

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -27,10 +27,27 @@ def update_readme(board, status):
 
     updated_board = ['&nbsp;' if tile == ' ' else tile for tile in board]
 
-    board_str = f"""| {updated_board[0]} | {updated_board[1]} | {updated_board[2]} |
-|---|---|---|
-| {updated_board[3]} | {updated_board[4]} | {updated_board[5]} |
-| {updated_board[6]} | {updated_board[7]} | {updated_board[8]} |"""
+    #test
+        cell_image = lambda cell: '/img/blank.png' if cell == ' ' else '/img/o.png' if cell == 'O' else '/img/x.png'
+    cell_type = lambda cell: 'X' if cell == 'X' else 'O' if cell == 'O' else 'Empty' if cell == ' ' else cell
+
+    board_str = f"""| Tic | Tac | Toe |
+|--|--|--|
+| [![{cell_type(updated_board[0])}]({cell_image(updated_board[0])})](https://github.com/Coding4Hours/tic_tac_toe/issues/new?title=move%201) | 
+[![{cell_type(updated_board[1])}]({cell_image(updated_board[1])})](https://github.com/Coding4Hours/tic_tac_toe/issues/new?title=move%202) | 
+[![{cell_type(updated_board[2])}]({cell_image(updated_board[2])})](https://github.com/Coding4Hours/tic_tac_toe/issues/new?title=move%203) |
+| [![{cell_type(updated_board[3])}]({cell_image(updated_board[3])})](https://github.com/Coding4Hours/tic_tac_toe/issues/new?title=move%204) | 
+[![{cell_type(updated_board[4])}]({cell_image(updated_board[4])})](https://github.com/Coding4Hours/tic_tac_toe/issues/new?title=move%205) | 
+[![{cell_type(updated_board[5])}]({cell_image(updated_board[5])})](https://github.com/Coding4Hours/tic_tac_toe/issues/new?title=move%206) |
+| [![{cell_type(updated_board[6])}]({cell_image(updated_board[6])})](https://github.com/Coding4Hours/tic_tac_toe/issues/new?title=move%207) | 
+[![{cell_type(updated_board[7])}]({cell_image(updated_board[7])})](https://github.com/Coding4Hours/tic_tac_toe/issues/new?title=move%208) | 
+[![{cell_type(updated_board[8])}]({cell_image(updated_board[8])})](https://github.com/Coding4Hours/tic_tac_toe/issues/new?title=move%209) |
+"""
+
+   # board_str = f"""| {updated_board[0]} | {updated_board[1]} | {updated_board[2]} |
+#|---|---|---|
+#| {updated_board[3]} | {updated_board[4]} | {updated_board[5]} |
+#| {updated_board[6]} | {updated_board[7]} | {updated_board[8]} |"""
 
     with open('stuff.json', 'r') as file:
         data = json.load(file)
