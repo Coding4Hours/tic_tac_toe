79b98cf1d0b62a8c961454a5672eeb2ed3dab528
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-07-08 18:31:47 -0700

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -33,11 +33,31 @@ def update_readme(board, status):
 | {board[3]} | {board[4]} | {board[5]} |
 | {board[6]} | {board[7]} | {board[8]} |"""
 
-    new_content = re.sub(r'## Current Board\n\n.*?\n\n', f'## Current Board\n\n{board_str}\n\n', content, flags=re.DOTALL)
-    new_content = re.sub(r'## Game Status\n\n.*', f'## Game Status\n\n{status}', new_content)
+    possible_moves = [i+1 for i, v in enumerate(board) if v == ' ']
+    moves_str = "Possible moves:\n\n"
+    for move in possible_moves:
+        issue_title = f"move {move}"
+        encoded_title = urllib.parse.quote(issue_title)
+        moves_str += f"- [Move {move}](../../issues/new?title={encoded_title})\n"
+        
+pythonCopyimport re
+import urllib.parse
 
-    with open('README.md', 'w') as file:
-        file.write(new_content)
+def update_readme(board, status):
+    with open('README.md', 'r') as file:
+        content = file.read()
+
+    board_str = f"""| {board[0]} | {board[1]} | {board[2]} |
+|---|---|---|
+| {board[3]} | {board[4]} | {board[5]} |
+| {board[6]} | {board[7]} | {board[8]} |"""
+
+    possible_moves = [i+1 for i, v in enumerate(board) if v == ' ']
+    moves_str = "Possible moves:\n\n"
+    for move in possible_moves:
+        issue_title = f"move {move}"
+        encoded_title = urllib.parse.quote(issue_title)
+        moves_str += f"- [Move {move}](../../issues/new?title={encoded_title})\n"
 
     new_content = re.sub(r'## Current Board\n\n.*?\n\n', f'## Current Board\n\n{board_str}\n\n', content, flags=re.DOTALL)
     new_content = re.sub(r'## Game Status\n\n.*', f'## Game Status\n\n{status}', new_content)
